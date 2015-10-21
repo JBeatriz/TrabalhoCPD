7d95233ba7223a927fbc4b4ac80bbce2867c9982
mauricio <mauricioagustoni@gmail.com>
2015-10-21 02:53:41 -0200

    inicio de arquivo
    iniciando implementação da classe de arquivamento e adição dos getters e
    setters da classe Eventos(por favor, comentem tudo, até os atomos do
    programa, não é feio ;) )

---
+++ src/trabalhocpd/ArquivoEventos.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalhocpd;
+
+/**
+ *
+ * @author Mauricio
+ */
+public class ArquivoEventos {
+    /*Aqui devemos implementar a leitura e escrita do arquivo
+    que guardará todos os eventos financeiros. Lembrando que 
+    a organização será por ordem de insersão, ou seja, ordem cronológica
+    adição ao arquivo. Deve haver uma arvore ou qualquer estrutura de
+    pesquisa que levará algum campo como chave de ordenamento e usará
+    apenas valores de indice do arquivo para apontar para o resto das
+    informações. Boa sorte rapeize ;)
+    */
+}
--- src/trabalhocpd/Evento.java
+++ src/trabalhocpd/Evento.java
@@ -21,6 +21,86 @@ public class Evento {
     private String info;
     private String data;
     private String valor;
-    private String tipo;
+    private char tipo;
     private String hora;
+    
+    //construtor da classe, o mais trivial possivel HUE
+    Evento(String info, String data, String valor, char tipo, String hora){
+        this.info = info;
+        this.data = data;
+        this.valor = valor;
+        this.tipo = tipo;
+        this.hora = hora;
+    } 
+
+    /**
+     * @return the info
+     */
+    public String getInfo() {
+        return info;
+    }
+
+    /**
+     * @return the data
+     */
+    public String getData() {
+        return data;
+    }
+
+    /**
+     * @return the valor
+     */
+    public String getValor() {
+        return valor;
+    }
+
+    /**
+     * @return the tipo
+     */
+    public char getTipo() {
+        return tipo;
+    }
+
+    /**
+     * @return the hora
+     */
+    public String getHora() {
+        return hora;
+    }
+
+    /**
+     * @param info the info to set
+     */
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    /**
+     * @param data the data to set
+     */
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    /**
+     * @param valor the valor to set
+     */
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    /**
+     * @param tipo the tipo to set
+     */
+    public void setTipo(char tipo) {
+        this.tipo = tipo;
+    }
+
+    /**
+     * @param hora the hora to set
+     */
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
 }
